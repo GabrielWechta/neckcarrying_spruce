b939f9f72c250988f8874cd12783955e7d8a3620
GabrielWechta <wechtagabriel@gmail.com>
2020-11-05 00:46:15 +0100

    need morning fix

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import numpy as np
-from datetime import date
+from datetime import date, timedelta
 import pandas as pd
 from pprint import pprint
 import requests
 from bs4 import BeautifulSoup
 
-dates = pd.date_range('20201101', periods=100)
-data_frame = pd.DataFrame(None, index=dates,
-                          columns=['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
-                                   'eleven', 'twelve'])
 today = date.today()
+path_to_csv = "/home/gabriel/Desktop/weather_conspiracy.csv"
+TWELVE_COLUMNS = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+                  'eleven', 'twelve']
+
+try:
+    data_frame = pd.read_csv(path_to_csv, index_col=0)
+
+except FileNotFoundError:
+    print("File at \'", path_to_csv, "\' couldn't be found. Creating new one...")
+
+    # creating procedure #
+    print(today)
+    dates = pd.date_range(today, periods=100)
+    data_frame = pd.DataFrame(None, index=dates,
+                              columns=TWELVE_COLUMNS)
 
 url = 'https://pogoda.interia.pl/prognoza-dlugoterminowa-szklarska-poreba,cId,34920'
 page = requests.get(url)
@@ -20,8 +31,9 @@ temperatures = soup.find_all(class_="table-temps")
 for temperature in temperatures:
     temps.append(temperature.text.strip().split('\n'))
 
-for i in range(12):
-    data_frame.iat[i, i] = temps[i]
+next_twelve_days = pd.date_range(today, periods=12)
+for i, (index, column) in enumerate(zip(next_twelve_days.date, TWELVE_COLUMNS)):
+    data_frame.at[index, column] = temps[i]
 
 print(data_frame.head())
 
