2cc01af3149b1cc822caef4266d55ca7f73ead15
GabrielWechta <wechtagabriel@gmail.com>
2020-12-16 01:12:23 +0100

    After fixing smal string bug and adding script to automate schedule after boot

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import numpy as np
-from datetime import date, timedelta
+from datetime import date
+
 import pandas as pd
-from pprint import pprint
 import requests
 from bs4 import BeautifulSoup
 
@@ -26,14 +25,12 @@ url = 'https://pogoda.interia.pl/prognoza-dlugoterminowa-szklarska-poreba,cId,34
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
-temps = []
 temperatures = soup.find_all(class_="table-temps")
-for temperature in temperatures:
-    temps.append(temperature.text.strip().split('\n'))
+temps = [temp.text.strip().split('\n') for temp in temperatures]
 
 next_twelve_days = pd.date_range(today, periods=12)
 for i, (index, column) in enumerate(zip(next_twelve_days.date, TWELVE_COLUMNS)):
-    data_frame.at[index, column] = temps[i]
+    data_frame.at[str(index), column] = temps[i]
 
 print(data_frame.head())
 
